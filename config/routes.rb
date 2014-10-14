fc07975e6f9a393e657e8c5191fe216a449539fc
Zak Koltun <zakkoltun@gmail.com>
2014-10-14 16:20:55 -0700

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 CafeOakville::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/menu"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
