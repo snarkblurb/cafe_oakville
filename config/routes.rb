4f068090dbc1a9fab62cdd3659ae128caac66057
Zak Koltun <zakkoltun@gmail.com>
2014-10-22 20:09:57 -0700

    Try fixing heroku/unicorn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 CafeOakville::Application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#home'
 	get 'menu' => 'static_pages#menu'
 	get 'about' => 'static_pages#about'
-	get '/signin' => 'users#new'
+	get 'signup' => 'users#new'
+	get 'login' => 'sessions#new'
+	post 'login' => 'sessions#create'
+	delete 'logout' => 'sessions#destroy'
 	resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
