d33fa4ea86f19d59b6b84dae4c1eb5723777e7a5
Zak Koltun <zakkoltun@gmail.com>
2014-10-25 03:30:16 -0700

    Add links to users in index, prevent users from editing other users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 	
 	before_action :logged_in_user, only: [:index, :edit, :update]
-	before_action :correct_user, only: [:show, :edit, :update]
+	before_action :correct_user, only: [:edit, :update]
 
 	def index
 		@users = User.all
