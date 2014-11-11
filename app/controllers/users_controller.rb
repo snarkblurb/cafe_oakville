ffedfaa0386f7493996bc29fbb4caed3cf79b603
Zak Koltun <zakkoltun@gmail.com>
2014-11-10 17:21:29 -0800

    Arrange buttons on reserve page, disable 'signed up', change colors to gold and red

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 	
 	before_action :logged_in_user, only: [:index, :show, :edit, :update]
-	before_action :correct_user, only: [:edit, :update]
+	before_action :correct_user, only: [:edit, :update, :show]
 
 
 	def index
