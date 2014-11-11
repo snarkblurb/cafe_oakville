efd875eec4916dea0a8697a700ebbe427cc47504
Zak Koltun <zakkoltun@gmail.com>
2014-11-11 00:15:11 -0800

    Create rough add event functionality

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,19 +1,37 @@
 class EventsController < ApplicationController
-	before_action :logged_in_user, only: [:index]
+	before_action :logged_in_user, only: [:index, :create, :destroy]
 
 	def index
 		@events = Event.all
 		@user = current_user
+		@event = @events.build if @user.admin?
 	end
 
-  def new
+  def create
+		@events = Event.all
+		@event = @events.build(event_params)
+		if @event.save
+			flash[:success] = "Event created!"
+			redirect_to events_path
+		else
+			render 'static_pages/home'
+		end
   end
-
+	
+	def destroy
+	end
 
 	private
+
+		def event_params
+			params.require(:event).permit(:day)
+		end
 		
 		def logged_in_user
-			redirect_to(login_url) unless logged_in?
+			if !logged_in?
+				flash[:danger] = "Please log in."
+				redirect_to(login_url) 
+			end
 		end
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,5 @@ class Event < ActiveRecord::Base
 	has_many :reservations, :dependent => :destroy
 	has_many :users, :through => :reservations
 
-	validates :day, presence: true;
+	validates :day, presence: true, uniqueness: true;
 end
